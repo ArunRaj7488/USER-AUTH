cf74a6fd6265a491ac824f623d474a7ab99a559c
ArunRaj7488 <arunraj1735169@gmail.com>
2021-02-25 08:41:36 +0530

    add user

--- utils/responseHandler.js
+++ utils/responseHandler.js
@@ -3,8 +3,9 @@ module.exports = ({res, error, message, data = null, status =200}) => {
     if(error){
         console.log(error);
         return res.status(status).send({
-            success: false,
-            error: error        })
+            success: "Something went wrong!",
+            error: error      
+        })
     } else{
         return res.status(status).send(
             !data? {
